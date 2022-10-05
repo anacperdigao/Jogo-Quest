5b8ca4e2c903dae5e53e3c5d166a1df99cf7ae2c
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-05 12:21:32 -0300

    Adicionando variaveis de cores

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -1,8 +1,7 @@
-export const vermelho = "#ED3305";
 export const cinzaClaro = "#F3F3F0";
-export const cinzaMedio = "#AEA59F";
-export const preto = "#080706";
-export const cinzaEscuro = "#66534E";
+export const azulEscuro = "#154A61";
+export const azulPiscina = "#92E1E2";
+export const azulEsverdeado = "#77B5A8";
+export const bege = "#F7CA90";
+export const rosa = "#F78672";
 export const laranja = "#E55315";
-export const marromClaro = "#D29265";
-export const marromEscuro = "#BF5F40";
