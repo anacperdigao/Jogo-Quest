743016b9647d359029cba78d3c76fac29a54889c
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-05 11:52:35 -0300

    Adicionando variaveis de cores

---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,8 @@
+export const vermelho = "#ED3305";
+export const cinzaClaro = "#F3F3F0";
+export const cinzaMedio = "#AEA59F";
+export const preto = "#080706";
+export const cinzaEscuro = "#66534E";
+export const laranja = "#E55315";
+export const marromClaro = "#D29265";
+export const marromEscuro = "#BF5F40";
